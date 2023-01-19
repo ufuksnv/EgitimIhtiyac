f977246b4ef34d5c141364f3bc3df9a51abd11cf
ufuk murat sınavu <u.murat0606@hotmail.com>
2023-01-19 22:06:23 +0300

    ...

--- EgitimIhtiyac/Program.cs
+++ EgitimIhtiyac/Program.cs
@@ -50,6 +50,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Need}/{action=Index}/{id?}");
 
 app.Run();
